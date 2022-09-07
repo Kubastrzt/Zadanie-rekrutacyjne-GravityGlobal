3e34c78afc456247dfca46559cb78cf9e6d8d839
Kubastrzt <kubastrzt@gmail.com>
2022-09-07 18:18:22 +0200

    Added modal with features

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+import content from './modal.js';
+
+const modalButton = document.querySelector('.popup-button');
+
+let counter = localStorage.getItem('counter');
+if (counter == null) {
+  counter = 0;
+}
+
+const popUp = document.createElement('div');
+const createPopUp = (content) => {
+  popUp.classList.add('alert');
+  popUp.innerHTML = content;
+  document.body.appendChild(popUp);
+};
+createPopUp(content);
+
+const resetButton = document.querySelector('.reset');
+const appendPopUp = () => {
+  popUp.classList.add('active');
+  setCounter();
+  if (localStorage.getItem('counter') > 4) {
+    resetButton.classList.add('active');
+  }
+};
+
+const resetCounter = () => {
+  localStorage.setItem('counter', 1);
+  counter = 1;
+  resetButton.classList.remove('active');
+  document.getElementById('counter').innerHTML =
+    getCounter() < 2 ? `${getCounter()} time` : `${getCounter()} times`;
+};
+
+const outsideClick = (e) => {
+  if (e.target == popUp) {
+    popUp.classList.remove('active');
+  }
+};
+
+const closePopUp = () => {
+  popUp.classList.remove('active');
+};
+
+const setCounter = () => {
+  localStorage.setItem('counter', ++counter);
+
+  document.getElementById('counter').innerHTML =
+    getCounter() < 2 ? `${getCounter()} time` : `${getCounter()} times`;
+};
+
+const getCounter = () => {
+  return localStorage.getItem('counter');
+};
+
+const closeButton = document.querySelector('.close-modal-button');
+closeButton.addEventListener('click', closePopUp);
+
+modalButton.addEventListener('click', appendPopUp);
+window.addEventListener('click', outsideClick);
+resetButton.addEventListener('click', resetCounter);
---
+++ src/js/modal.js
@@ -0,0 +1,10 @@
+const content = `
+    <div class="pop-up">
+        <img class="close-modal-button" src="../../images/x-icon.svg" alt="Close modal window">
+    <h1 class="title">Alert!</h1>
+    <p class="description">
+      You have clicked <span id="counter">X times</span> to related button.
+    </p>
+    <button class="reset popup-button" aria-label="Reset counter">Reset</button>
+  </div>`;
+export default content;
