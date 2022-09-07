0558b96a8fbcc224a66b1e3a990be07cffbee5b6
Kubastrzt <kubastrzt@gmail.com>
2022-09-07 19:11:57 +0200

    Added tabindexes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,10 @@
 const content = `
     <div class="pop-up">
-        <img class="close-modal-button" src="../../images/x-icon.svg" alt="Close modal window">
-    <h1 class="title">Alert!</h1>
-    <p class="description">
-      You have clicked <span id="counter">X times</span> to related button.
+    <button class="close-modal-button" aria-label="Close modal window" tabindex="8"></button>
+    <h1 class="title" tabindex="5">Alert!</h1>
+    <p class="description" tabindex="6">
+      You have clicked <strong><span id="counter">X times</span></strong> to related button.
     </p>
-    <button class="reset popup-button" aria-label="Reset counter">Reset</button>
+    <button class="reset popup-button" aria-label="Reset counter" tabindex="7">Reset</button>
   </div>`;
 export default content;
