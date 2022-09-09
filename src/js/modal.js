e7ad7995fe6b7e0b741da0dd498ac3524ed55fc7
Kubastrzt <kubastrzt@gmail.com>
2022-09-09 12:02:46 +0200

    Improved semantics

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,7 @@
 const content = `
     <div class="pop-up">
     <button class="close-modal-button" aria-label="Close modal window" tabindex="8"></button>
-    <h1 class="title" tabindex="5">Alert!</h1>
+    <h2 class="title" tabindex="5">Alert!</h2>
     <p class="description" tabindex="6">
       You have clicked <strong><span id="counter">X times</span></strong> to related button.
     </p>
